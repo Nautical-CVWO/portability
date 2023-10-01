c088de9580f313088543fd258dfb5404bcf9537e
lamchenghou <lamchenghouu@gmail.com>
2023-10-01 17:40:20 +0800

    Rename header

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -24,7 +24,7 @@ import { User } from "./Homepage"
 import { ref, onValue, DataSnapshot } from "firebase/database";
 import { db } from '../backend/firebase';
 
-const linkStyle = { 
+const linkStyle = {
   textDecoration: "none", // Remove underline
   color: "white",
 };
@@ -59,22 +59,22 @@ const dropDownStyle = {
   padding: '0px',
 };
 
-const Header = ({user, setUser, isAdmin = false}: HeaderProps) => {
-  const navItems = isAdmin ? ["Home", "Employee Survey", "Workshop", "Dashboard"] : ["Home", "Workshop", "Dashboard"];
+const Header = ({ user, setUser, isAdmin = false }: HeaderProps) => {
+  const navItems = isAdmin ? ["Home", "Assess Employees", "Workshop", "Dashboard"] : ["Home", "Workshop", "Dashboard"];
   const linkItems = isAdmin ? [
     "/",
     "/employee_survey",
     '/workshop',
     "https://nautical-analytics.streamlit.app/",
-  ] : 
-  [
-    "/",
-    '/workshop',
-    "https://nautical-analytics.streamlit.app/",
-  ];
-  const handleDrawerToggle = () => {};
+  ] :
+    [
+      "/",
+      '/workshop',
+      "https://nautical-analytics.streamlit.app/",
+    ];
+  const handleDrawerToggle = () => { };
   // const location = useLocation();
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [userSelectedValue, setUserSelectedValue] = useState("points");
   const handleUserSelectionChange = (event: SelectChangeEvent) => {
     setUserSelectedValue(event.target.value);
@@ -85,8 +85,8 @@ const Header = ({user, setUser, isAdmin = false}: HeaderProps) => {
   useEffect(() => {
     setUserSelectedValue("points");
     const reference = ref(db, `employees/${user?.uid}/`);
-  
-    const onDataChange = (snapshot : DataSnapshot) => {
+
+    const onDataChange = (snapshot: DataSnapshot) => {
       if (snapshot.exists()) {
         const userData = snapshot.val();
         if (userData && userData.points !== undefined) {
@@ -94,24 +94,24 @@ const Header = ({user, setUser, isAdmin = false}: HeaderProps) => {
         }
       }
     };
-  
+
     // Set up a real-time listener for changes
     const unsubscribe = onValue(reference, onDataChange);
-  
+
     // Return a cleanup function to remove the listener when the component unmounts
     return () => {
       unsubscribe(); // Remove the real-time listener
     };
-  
+
   }, [user]);
-  
+
 
   const logout = () => {
     logoutData().then((res) => {
       alert(res)
-      
+
     }).then(() => navigate('/')).then(() => navigate(0))
-    
+
   }
 
   return (
@@ -128,8 +128,8 @@ const Header = ({user, setUser, isAdmin = false}: HeaderProps) => {
           >
             <MenuIcon />
           </IconButton >
-          
-          <a href="/"><img src={naut_logo} alt="logo" height="40px" width="40px" style={{ marginRight: '5px' }}/></a>
+
+          <a href="/"><img src={naut_logo} alt="logo" height="40px" width="40px" style={{ marginRight: '5px' }} /></a>
           <Typography
             variant="h6"
             component="a"
@@ -137,9 +137,9 @@ const Header = ({user, setUser, isAdmin = false}: HeaderProps) => {
             href="/"
           >NAUTICAL
           </Typography>
-            
-            
- 
+
+
+
           {user === undefined ? <></> : (
             <FormControl sx={{ padding: '0px' }}>
               {/* <InputLabel>Welcome, {user} you have {points} </InputLabel> */}
@@ -147,31 +147,31 @@ const Header = ({user, setUser, isAdmin = false}: HeaderProps) => {
                 value={userSelectedValue}
                 onChange={handleUserSelectionChange}
                 sx={dropDownStyle}
-              > 
-                  <MenuItem value="points" sx={menuItemStyle}>
+              >
+                <MenuItem value="points" sx={menuItemStyle}>
                   <div style={{
-                    display: "flex", 
+                    display: "flex",
                     alignItems: "center",
                     justifyContent: "center"
                   }}>
                     <div>
-                        {`Welcome, ${user?.username} you have ${userPoints}`.toUpperCase()}
+                      {`Welcome, ${user?.username} you have ${userPoints}`.toUpperCase()}
                     </div>
                     <div>
-                      <Inventory2RoundedIcon sx={{marginLeft: '5px', fontSize: '14px', marginTop: '5px'}} />
+                      <Inventory2RoundedIcon sx={{ marginLeft: '5px', fontSize: '14px', marginTop: '5px' }} />
                     </div>
                   </div>
-                  </MenuItem>
-                
-                  <MenuItem onClick={() => {
-                    setUser(undefined);
-                    logout();
-                  }} value="logout" sx={menuItemStyle}>LOGOUT</MenuItem>
+                </MenuItem>
+
+                <MenuItem onClick={() => {
+                  setUser(undefined);
+                  logout();
+                }} value="logout" sx={menuItemStyle}>LOGOUT</MenuItem>
               </Select>
             </FormControl>
-            
+
           )}
-      
+
           <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((index, item) => (
               <Button
@@ -187,7 +187,7 @@ const Header = ({user, setUser, isAdmin = false}: HeaderProps) => {
               </Button>
             ))}
           </Box>
-          
+
         </Toolbar>
       </AppBar>
       <nav></nav>
